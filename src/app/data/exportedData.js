70662ac9a682a65884b54c688b25dd782fbd4b69
Jessica <jessikareix20@gmail.com>
2023-07-09 01:34:09 +0200

    I moved  card data to data folder

--- src/app/data/exportedData.js
+++ src/app/data/exportedData.js
@@ -1,20 +1,18 @@
 
-
 import dataMayores from '../data/arcanos-mayores.json';
 import dataMenores from '../data/arcanos-menores.json';
 
-
 const MAYORES = dataMayores.Arcanos.map(arcano => ({
-    nombre: arcano.nombre,
-    normal: arcano.normal,
-    invertida: arcano.invertida
+  nombre: arcano.nombre,
+  normal: arcano.normal,
+  invertida: arcano.invertida
 }));
 const MENORES = Object.keys(dataMenores).map(key => ({
-    nombre: dataMenores[key].nombre,
-    normal: dataMenores[key].normal,
-    invertida: dataMenores[key].invertida
-  }));
-  
+  nombre: dataMenores[key].nombre,
+  normal: dataMenores[key].normal,
+  invertida: dataMenores[key].invertida
+}));
+
 
 
 let ARCANOS = { ...MAYORES, ...MENORES };
